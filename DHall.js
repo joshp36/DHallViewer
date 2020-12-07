2ce264e303396cf53aaf752db7e7dc34d90b657d
joshp36 <61866678+joshp36@users.noreply.github.com>
2020-12-06 23:36:53 -0600

    added DHall.js

---
+++ DHall.js
@@ -0,0 +1,21 @@
+
+    var DHall = {
+      Image: {
+    xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+    Url: "//joshp36.github.io/DHallViewer/DHall_pieces/",
+    Format: "png",
+    Overlap: "1",
+    TileSize: "256",
+    Size: {
+      Width:  "41618",
+      Height: "14090"
+    }
+  }
+};
+    
+    var viewer = OpenSeadragon({
+      id: "seadragon-viewer",
+      prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+      tileSources: DHall,
+      showNavigator:  false
+    });
